b6e39b931f7e4ea8cf7f57a6772f0c99549bdf9a
Елена Закерничная <svintusiatko@gmail.com>
2017-06-01 16:15:19 +0300

    Revert "InitialCommit"
    This reverts commit 306a4fa4faf393626d739b988db8bd0b1fdef604.

--- AuthServerNew/src/com/dreamlex/exceptions/CardFileException.java
+++
@@ -1,8 +0,0 @@
-package com.dreamlex.exceptions;
-
-@SuppressWarnings("serial")
-public class CardFileException extends Exception {
-	public CardFileException(String message) {
-		super(message);
-	}
-}
